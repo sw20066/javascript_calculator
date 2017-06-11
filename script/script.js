458013a1ae5b5e259cfd55ee8323692b6dc0f572
Spencer Wei <Spencer>
2017-06-11 14:22:44 -0700

    Added clear and refinements

--- script/script.js
+++ script/script.js
@@ -1,35 +1,49 @@
 // ------------------- Setting Up Variables ------------------- //
-var previousAnswer = [];
+var previousAnswers = [];
 var currentInput = ""
-var temp = 0;
+var temp;
 // ------------------- Setting Up Functions ------------------- //
 var operators = {
-        plus: (a, b) => a + b
-        , minus: (a, b) => a - b
-        , divide: (a, b) => a / b
-        , multiply: (a, b) => a * b
-    }
-    // -------------------   Core Functions    ------------------- //
-$("button").click(function () {
-    if ($(this).hasClass("number")) {
-        currentInput += $(this).val();
-        $("#display").val(currentInput);
-    }
-    else if ($(this).hasClass("basic-operators")) {
-        currentInput += ` ${$(this).val()} `
-        $("#display").val(currentInput);
-    }
-    else if ($(this).hasClass("equal")) {
+    plus: (a, b) => a + b,
+    minus: (a, b) => a - b,
+    divide: (a, b) => a / b,
+    multiply: (a, b) => a * b
+}
+var clear_operators = {
+    clearAll: () => {
+        currentInput = "";
+        $("#display").val(currentInput)
+    },
+    clearCurrent: () => {
         currentInput = currentInput.split(" ");
-        calculateAnswer()
+        currentInput.pop();
+        currentInput = currentInput.join(" ") + " ";
+        $("#display").val(currentInput)
+    },
+    backSpace: () => {
+        console.log("Backspacing")
     }
-    else if ($(this).hasClass("clear-operations")) {
-        if ($(this).val() === "clearAll") {
-            currentInput = [];
-            $("#display").val(currentInput)
-        };
+}
+var special_operators = {
+    convert: () => {
+        console.log("converting...");
+    },
+    sqrt: () => {
+        console.log("Square rooting...");
+    },
+    pow: () => {
+        console.log("Power")
+    },
+    pow3: () => {
+        console.log("UNLIMITED POWER");
+    },
+    reciprocal: () => {
+        console.log("Reciporocal");
+    }, 
+    decimal: () => {
+        console.log("Decimal Operator");
     }
-});
+}
 var calculateAnswer = () => {
     var calculatingOps = (operator) => {
         temp = currentInput.splice(i - 1, 3);
@@ -38,19 +52,37 @@ var calculateAnswer = () => {
         i = i - 2;
     }
     for (var i = 0; i < currentInput.length; i++) {
-        if (currentInput[i] === "+") {
-            calculatingOps("plus")
-        }
-        else if (currentInput[i] === "-") {
-            calculatingOps("minus")
-        }
-        else if (currentInput[i] === "x") {
-            calculatingOps("multiply")
-        }
-        else if (currentInput[i] === "/") {
-            calculatingOps("divide")
+        switch (currentInput[i]) {
+            case "+":
+                calculatingOps("plus");
+                break;
+            case "-":
+                calculatingOps("minus");
+                break;
+            case "x":
+                calculatingOps("multiply");
+                break;
+            case "/":
+                calculatingOps("divide");
         }
     }
     $("#display").val(temp);
     currentInput = "" + temp;
 };
+// -------------------   Core Functions    ------------------- //
+$("button").click(function () {
+    if ($(this).hasClass("number")) {
+        currentInput += $(this).val();
+        $("#display").val(currentInput);
+    } else if ($(this).hasClass("basic-operators")) {
+        currentInput += ` ${$(this).val()} `
+        $("#display").val(currentInput);
+    } else if ($(this).hasClass("equal")) {
+        currentInput = currentInput.split(" ");
+        calculateAnswer()
+    } else if ($(this).hasClass("clearing-operators")) {
+        clear_operators[$(this).val()]();
+    } else if ($(this).hasClass("special-operators")) {
+        special_operators[$(this).val()]();
+    }
+});
